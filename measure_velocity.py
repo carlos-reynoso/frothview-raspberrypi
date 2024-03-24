6fc415ca9c2f749ad90cdbc821a8a6e45e33ab38
Carlos Reynoso <64491046+carlos-reynoso@users.noreply.github.com>
2024-03-24 13:01:49 -0600

    removing color maps

--- measure_velocity.py
+++ measure_velocity.py
@@ -2,19 +2,13 @@ import cv2
 import numpy as np
 import time
 
-def color_map(velocity, max_velocity):
-    normalized_velocity = min(velocity / max_velocity, 1)
-    blue_component = int(normalized_velocity * 255)
-    red_component = int((1 - normalized_velocity) * 255)
-    return red_component, 0, blue_component
-
-def draw_flow(img, flow, winsize=30, real_time_fps=30, show_fps=False, frame_count=0, start_time=None, prev_avg_velocity=0, alpha=0.1):
+def draw_flow(img, flow, winsize=30, real_time_fps=30, show_fps=False, frame_count=0, start_time=None, prev_avg_velocity=0, alpha=0.1, conv_factor=1.0):
     h, w = img.shape[:2]
     step = max(2, winsize // 2)  # Dynamically set step based on winsize
     y, x = np.mgrid[step/2:h:step, step/2:w:step].reshape(2, -1).astype(np.int16)
 
     fx, fy = flow[y, x].T
-    velocities = np.sqrt(fx**2 + fy**2) * real_time_fps *1
+    velocities = np.sqrt(fx**2 + fy**2) * real_time_fps * conv_factor
     current_velocity = np.mean(velocities) if velocities.size > 0 else 0
 
     # Calculate the Exponential Moving Average for velocity
@@ -24,9 +18,8 @@ def draw_flow(img, flow, winsize=30, real_time_fps=30, show_fps=False, frame_cou
     lines = np.int16(lines + 0.5)
     
     vis = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-    for ((x1, y1), (x2, y2)), velocity in zip(lines, velocities):
-        color = color_map(velocity, 2)
-        cv2.line(vis, (x1, y1), (x2, y2), color, 1)
+    for (x1, y1), (x2, y2) in lines:
+        cv2.line(vis, (x1, y1), (x2, y2), (0, 255, 0), 1)  # Draw lines in green
 
     if show_fps and start_time is not None:
         current_fps = frame_count / (time.time() - start_time)
@@ -35,9 +28,16 @@ def draw_flow(img, flow, winsize=30, real_time_fps=30, show_fps=False, frame_cou
 
     return vis, avg_velocity
 
-
-
 def main(show_fps=False, use_roi=True, scale_factor=1.0):
+    # Read conversion factor from conv_factor.txt, if it exists
+    try:
+        with open('conv_factor.txt', 'r') as file:
+            conv_factor = float(file.read())
+            print(f'Using conversion factor of {conv_factor}')
+    except FileNotFoundError:
+        print('Conversion factor not found, using default conversion factor of 1')
+        conv_factor = 1
+
     cap = cv2.VideoCapture(0)
     ret, prev = cap.read()
 
@@ -46,8 +46,6 @@ def main(show_fps=False, use_roi=True, scale_factor=1.0):
         return
 
     frame_height, frame_width = prev.shape[:2]
-
-    # Calculate the ROI based on scale factor
     w_roi = int(frame_width * scale_factor)
     h_roi = int(frame_height * scale_factor)
     x_roi = (frame_width - w_roi) // 2
@@ -75,16 +73,15 @@ def main(show_fps=False, use_roi=True, scale_factor=1.0):
             roi_frame = frame
 
         gray = cv2.cvtColor(roi_frame, cv2.COLOR_BGR2GRAY)
-
-        # Ensure prev_gray is only set after the first frame is captured
         if frame_count == 0:
             prev_gray = gray
 
+        flow = cv2.calcOpticalFlowFarneback(
+            prev_gray, gray, None, 0.5, 1, winsize, 5, 5, 1.2, 0)
 
-        flow = cv2.calcOpticalFlowFarneback(prev_gray, gray, None, 0.5, 1, winsize, 5, 5, 1.2, 0)
         prev_gray = gray.copy()  # Update prev_gray for the next iteration
 
-        vis, prev_avg_velocity = draw_flow(gray, flow, winsize, real_time_fps, show_fps, frame_count, start_time, prev_avg_velocity)
+        vis, prev_avg_velocity = draw_flow(gray, flow, winsize, real_time_fps, show_fps, frame_count, start_time, prev_avg_velocity, conv_factor)
         cv2.imshow('Optical flow', vis)
 
         frame_count += 1
@@ -92,9 +89,8 @@ def main(show_fps=False, use_roi=True, scale_factor=1.0):
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-
     cap.release()
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
-    main(show_fps=True, use_roi=True, scale_factor=.5)  # Adjust scale_factor as needed
+    main(show_fps=True, use_roi=True, scale_factor=0.5)  # Adjust scale_factor
